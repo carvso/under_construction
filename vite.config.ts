80dc69bfb908d7f1a4f080f7f570746d604daf56
carvso <vcarusobusiness@gmail.com>
2025-03-11 16:41:07 +0100

    updatevite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: process.env.GITHUB_PAGES ? '/under_construction/' : '/', // Replace 'your-repo-name' with your actual repository name
+  base: './', // This ensures assets are loaded relative to the index.html
 });
